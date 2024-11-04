246fee70e2bd3ba294fc631a0ace0ff3515cb92f
German <german.fabregasvacas@usp.ceu.es>
2024-11-04 13:46:13 +0100

    Añado JSON con diccionario al ej3

--- Ejercicio3/server.js
+++ Ejercicio3/server.js
@@ -2,11 +2,19 @@ const http = require('http');
 const url = require('url');
 const fs = require('fs');
 
+// Cargar el diccionario de palabras desde diccionario.json
 // Palabras del ejercicio anterior cambiando las que tenía tildes por similares
-const palabras = [
-  'electricidad', 'integral', 'triunfador', 'aplaudir', 'remitente', 'asfixiar', 'periodista', 'aguacero', 
-  'freno', 'catedral'
-];
+let palabras = [];
+
+// Leer el archivo JSON 
+fs.readFile('./diccionario.json', 'utf-8', (err, data) => {
+  if (err) {
+    console.error('Error al leer el archivo palabras.json:', err);
+    return;
+  }
+  // Parsear el contenido JSON y asignar las palabras al array
+  palabras = JSON.parse(data).palabras;
+});
 
 // Función para generar una contraseña aleatoria
 function generarContraseña(numPalabras) {
